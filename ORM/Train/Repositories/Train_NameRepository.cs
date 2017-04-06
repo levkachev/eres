583206e1379a45e2847e63ee7b0bcf526d0abdde
Marina Vasilieva <marina_paley@mail.ru>
2017-04-06 22:08:08 +0300

    06.04.2017

--- ORM/Train/Repositories/Train_NameRepository.cs
+++ ORM/Train/Repositories/Train_NameRepository.cs
@@ -1,24 +1,39 @@
 ﻿using ORM.Base;
-using NHibernate;
 using ORM.Train.Entities;
 using System.Linq;
-using System.Collections.Generic;
 using System;
 
 namespace ORM.Train.Repositories
 {
-   public class Train_NameRepository : Repository<Train_Name>
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Train_NameRepository : Repository<Train_Name>
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public static Train_NameRepository GetInstance()
         {
             return GetInstance<Train_NameRepository>(SessionWrapper.GetInstance().Factory);
         }
-
-        public Train_Name GetByName(String name)
+        public Guid GetIDByName(String name)
         {
-            return GetAll()
-                .Where(tname => tname.Name == name)
-                .SingleOrDefault();
+            var tmp = GetAll()
+                .SingleOrDefault(tr => tr.Name == name);
+            if (tmp == null)
+                throw new ArgumentOutOfRangeException(paramName: nameof(name));
+            return tmp.ID;
+        }
+
+        public Motor_Type GetIDMotorTypeByName(String name)
+        {
+            var tmp = GetAll()
+                .SingleOrDefault(tr => tr.Name == name);
+            if (tmp == null)
+                throw new ArgumentOutOfRangeException(paramName: nameof(name));
+            return tmp.MotorType;
         }
     }
 }
