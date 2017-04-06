24e43504f4798bc7dd1e4e8ee2fd395de99ad582
Marina Vasilieva <marina_paley@mail.ru>
2017-04-06 22:17:56 +0300

    06.04.2017

---
+++ TrainMovement/Train/ACTrain.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ORM.Machine;
+using ORM.Train.Entities;
+
+namespace TrainMovement.Train
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ACTrain : BaseTrain
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private Int32 nbAuto;
+
+        /// <summary>
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException" accessor="set">less zero.</exception>
+        public Int32 NbAuto
+        {
+            get { return nbAuto; }
+            set {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                nbAuto = value;
+            }
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="machine"></param>
+        /// <param name="commonProperties"></param>
+        /// <param name="trainName"></param>
+        /// <exception cref="ArgumentException">Train.Name != <paramref name="commonProperties"/>.TrainName</exception>
+        /// <exception cref="ArgumentNullException">value is <see langword="null"/></exception>
+        /// <exception cref="ArgumentOutOfRangeException">empty.</exception>
+        internal ACTrain(ACMachine machine, ACParametres commonProperties, String trainName)
+        {
+            Name = trainName;
+            
+            if (!Name.Equals(commonProperties.Name))
+                throw new ArgumentException("Train.Name != commonProperties.TrainName");
+            NbAuto = commonProperties.NBAuto;
+
+            if (!Name.Equals(machine.Name))
+                throw new ArgumentException("Train.Name != machine.TrainName");
+            Machine = machine;
+            
+        }
+    }
+}
