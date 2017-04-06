492f8f33169aa07f63e0dc742f4fc5680ed3b7d5
Marina Vasilieva <marina_paley@mail.ru>
2017-04-06 22:22:18 +0300

    06.04.2017

---
+++ ORM/Machine/ACMachine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ORM.Machine
+{
+    /// <summary>
+    /// Параметры для AC двигателя 
+    /// </summary>
+    public class ACMachine : BaseMachine
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private Double umax;
+
+        /// <summary>
+        /// Номинальное напряжение двигателя
+        /// </summary>
+        private Double uNominal;
+
+        /// <summary>
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException" accessor="set">less zero.</exception>
+        public Double Umax
+        {
+            get { return umax; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                umax = value;
+            }
+        }
+
+        /// <summary>
+        /// Номинальное напряжение двигателя FLOAT NOT NULL
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException" accessor="set">less zero</exception>
+        public Double Unominal
+        {
+            get { return uNominal; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                uNominal = value;
+            }
+        }
+    }
+}
