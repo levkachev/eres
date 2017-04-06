d919d88514eb5b8f903dfd53a050157eb921d4e1
Marina Vasilieva <marina_paley@mail.ru>
2017-04-06 22:17:13 +0300

    06.04.2017

--- ORM/Train/Repositories/АdditionalParameterRepository.cs
+++ ORM/Train/Repositories/АdditionalParameterRepository.cs
@@ -1,28 +1,167 @@
 ﻿using ORM.Base;
-using NHibernate;
 using ORM.Train.Entities;
 using System.Linq;
-using System.Collections.Generic;
 using System;
+using System.Collections.Generic;
+using ORM.Machine;
 
 
 namespace ORM.Train.Repositories
 {
+    /// <summary>
+    /// Класс для создания объектов двигателя и поезда
+    /// </summary>
     public class АdditionalParameterRepository : Repository<АdditionalParameter>
     {
+        /// <summary>
+        /// Создает экземляр репозиторий для дополнительных параметров
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">factory is <see langword="null"/></exception>
         internal static АdditionalParameterRepository GetInstance()
         {
             return GetInstance<АdditionalParameterRepository>(SessionWrapper.GetInstance().Factory);
         }
 
         /// <summary>
-        /// 
+        /// Достает из БД все записи из таблицы Дополнительные параметры
         /// </summary>
         /// <returns></returns>
-        public static АdditionalParameter GetАdditionalParameter()
+        /// <exception cref="ArgumentNullException">Параметр <paramref name="source" /> имеет значение null.</exception>
+        private static IList<АdditionalParameter> GetАdditionalParameter() => АdditionalParameterRepository.GetInstance().GetAll();
+
+        /// <summary>
+        /// Создает объект для AC поезда с дополнительными параметрами
+        /// </summary>
+        /// <param name="trainName"></param>
+        /// <returns></returns>
+        public static ACParametres GetACTrainParametres(String trainName)
         {
             var repository = АdditionalParameterRepository.GetInstance();
-            return repository.GetAll().FirstOrDefault();
+
+            var baseParametres = repository.GetTrainBaseParametres(trainName);
+
+            var acTrainParametres = (ACParametres)baseParametres;
+
+            acTrainParametres.NBAuto = repository.GetAdditionalParametresByTrainName(trainName).nbAuto;
+
+            return acTrainParametres;
+        }
+        /// <summary>
+        /// Создает объект для DC поезда с дополнительными параметрами (Не сделан!)
+        /// </summary>
+        /// <param name="trainName"></param>
+        /// <returns></returns>
+        public static DСParametres GetDCTrainParametres(String trainName)
+        {
+            var repository = АdditionalParameterRepository.GetInstance();
+
+            var baseParametres = repository.GetTrainBaseParametres(trainName);
+
+            var dcTrainParametres = (DСParametres)baseParametres;
+
+            //это заглушка. Дописать все свойства!
+
+            return dcTrainParametres;
+        }
+
+        /// <summary>
+        /// Создает объект для  AC двигателя
+        /// </summary>
+        /// <param name="trainName"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException">less zero.</exception>
+        public static ACMachine GetACMachineParametres(String trainName)
+        {
+            var repository = АdditionalParameterRepository.GetInstance();
+
+            var baseMachine = repository.GetBaseMachineParametres(trainName);
+
+            var acMachine = (ACMachineParametres)baseMachine;
+
+            acMachine.Umax = repository.GetAdditionalParametresByTrainName(trainName).Umax;
+            acMachine.Unominal = repository.GetAdditionalParametresByTrainName(trainName).Unom;
+
+            return acMachine;
+
+        }
+
+        /// <summary>
+        /// Создает объект для  DC двигателя (Не сделан!)
+        /// </summary>
+        /// <param name="trainName"></param>
+        /// <returns></returns>
+        public static DCMachine GetDCMachineParametres(String trainName)
+        {
+            var repository = АdditionalParameterRepository.GetInstance();
+
+            var baseMachine = repository.GetBaseMachineParametres(trainName);
+
+            var dcMachine = (DCMachineParametres)baseMachine;
+
+            //Это заглушка!!! Вынуть из БД все параметры
+
+            return dcMachine;
+
+        }
+
+        /// <summary>
+        /// Создает объект для общего двигателя
+        /// </summary>
+        /// <param name="trainName"></param>
+        /// <returns></returns>
+        private BaseMachine GetBaseMachineParametres(String trainName)
+        {
+            var assemblyPullTime = GetAdditionalParametresByTrainName(trainName).AssemblyPullTime;
+            var disassemblyPowerCircuitTime = GetAdditionalParametresByTrainName(trainName).AssemblyBreakResistance;
+            var assemblyBreakTime = GetAdditionalParametresByTrainName(trainName).AssemblyBreakTime;
+            return new MachineBaseParametres(assemblyPullTime, assemblyBreakTime, assemblyPullTime, trainName);
+        }
+
+        /// <summary>
+        /// Создает объект для общего поезда  с дополнительными параметрами 
+        /// </summary>
+        /// <param name="trainName"></param>
+        /// <returns></returns>
+        private TrainBaseParametres GetTrainBaseParametres(String trainName)
+        {
+            var carLength = GetAdditionalParametresByTrainName(trainName).CarLength;
+
+            var unladenWeight = GetAdditionalParametresByTrainName(trainName).UnladenWeight;
+
+            var numberCars = GetAdditionalParametresByTrainName(trainName).NumberCars;
+
+            var breakAverage = GetAdditionalParametresByTrainName(trainName).NumberCars;
+
+            var netResistencePullFactor = GetAdditionalParametresByTrainName(trainName).NetResistencePullFactor;
+
+            var aerodynamicDragFactor = GetAdditionalParametresByTrainName(trainName).AerodynamicDragFactor;
+
+            var netResistenceCoastingFactor1 = GetAdditionalParametresByTrainName(trainName).NetResistenceCoastingFactor1;
+
+            var netResistenceCoastingFactor2 = GetAdditionalParametresByTrainName(trainName).NetResistenceCoastingFactor2;
+
+            var netResistenceCoastingFactor3 = GetAdditionalParametresByTrainName(trainName).NetResistenceCoastingFactor3;
+
+            var trainEqvivalentSurface = GetAdditionalParametresByTrainName(trainName).TrainEqvivalentSurface;
+
+            var inertiaRotationFactor = GetAdditionalParametresByTrainName(trainName).InertiaRotationFactor;
+
+            var ownNeedsElectricPower = GetAdditionalParametresByTrainName(trainName).OwnNeedsElectricPower;
+
+            return new TrainBaseParametres(numberCars, carLength, unladenWeight, breakAverage, netResistencePullFactor, aerodynamicDragFactor, netResistenceCoastingFactor1, netResistenceCoastingFactor2, netResistenceCoastingFactor3, trainEqvivalentSurface, inertiaRotationFactor, ownNeedsElectricPower);
+
+        }
+
+        /// <summary>
+        /// Достает из БД все дополнительные параметры по имени поезда
+        /// </summary>
+        /// <param name="trainName"></param>
+        /// <returns></returns>
+        private АdditionalParameter GetAdditionalParametresByTrainName(String trainName)
+        {
+            return GetAll()
+              .SingleOrDefault(baseTrain => baseTrain.TrainName.Name == trainName);
         }
     }
 }
