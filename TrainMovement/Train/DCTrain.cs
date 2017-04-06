b70e8937875663becc9cf591385498ad27f640fe
Marina Vasilieva <marina_paley@mail.ru>
2017-04-06 22:18:50 +0300

    06.04.2017

---
+++ TrainMovement/Train/DCTrain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ORM.Machine;
+using ORM.Train.Entities;
+
+namespace TrainMovement.Train
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DCTrain : BaseTrain
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="machine"></param>
+        /// <param name="commonProperties"></param>
+        /// <param name="trainName"></param>
+        /// <exception cref="ArgumentException">Train.Name != <paramref name="commonProperties"/>.TrainName</exception>
+        /// <exception cref="ArgumentNullException">value is <see langword="null"/></exception>
+        /// <exception cref="ArgumentOutOfRangeException">empty.</exception>
+        internal DCTrain(DCMachine machine, DСParametres commonProperties, String trainName)
+        {
+            Name = trainName;
+
+            if (!Name.Equals(commonProperties.Name))
+                throw new ArgumentException("Train.Name != commonProperties.TrainName");
+
+            if (!Name.Equals(machine.Name))
+                throw new ArgumentException("Train.Name != machine.TrainName");
+            Machine = machine;
+
+        }
+    }
+}
