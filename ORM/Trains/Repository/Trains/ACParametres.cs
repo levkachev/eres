88dcd2d210326e102312ab361ba54c14ac5b5358
Marina Vasilieva <marina_paley@mail.ru>
2017-04-16 20:12:51 +0300

    Удалили MashineBaseParametres. Он лишний и нигде не использовался. Опять переместила  файлы AC, DC, BaseTrainParametres в Repositori

--- ORM/Trains/Repository/Trains/ACParametres.cs
+++ ORM/Trains/Repository/Trains/ACParametres.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 
-namespace ORM.Train.Entities
+namespace ORM.Trains.Repository.Trains
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class ACParametres : BaseTrainParametres
     {
+        /// <summary>
+        /// 
+        /// </summary>
         private Int32 nbAuto;
 
+        /// <summary>
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException" accessor="set">Condition.</exception>
         public Int32 NBAuto
         {
             get { return nbAuto; }
