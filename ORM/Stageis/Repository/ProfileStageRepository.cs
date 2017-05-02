ed0e36effbafb817785401721df00b52788dbb96
Marina Vasilieva <marina_paley@mail.ru>
2017-05-02 08:44:37 +0300

    new

---
+++ ORM/Stageis/Repository/ProfileStageRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ORM.Base;
+using ORM.Stageis.Entities;
+
+namespace ORM.Stageis.Repository
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ProfileStageRepository : Repository<ProfileStage>
+    {
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">factory is <see langword="null"/></exception>
+        public static ProfileStageRepository GetInstance()
+        {
+            return GetInstance<ProfileStageRepository>(SessionWrapper.GetInstance().Factory);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stage"></param>
+        /// <returns></returns>
+        public IEnumerable<ProfileStage> GetLimits(Guid stage)
+        {
+
+            return GetAll()
+                .Where(st => st.Stage.ID == stage)
+                .Select(st => new ProfileStage());
+        }
+    }
+}
