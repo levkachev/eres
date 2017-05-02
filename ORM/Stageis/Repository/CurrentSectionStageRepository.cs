2eb592f4e88103413b066739acf1b97b269584b9
Marina Vasilieva <marina_paley@mail.ru>
2017-05-02 08:44:19 +0300

    new

---
+++ ORM/Stageis/Repository/CurrentSectionStageRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ORM.Base;
+using ORM.Stageis.Entities;
+
+namespace ORM.Stageis.Repository
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CurrentSectionStageRepository : Repository<CurrentSectionStage>
+    {
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">factory is <see langword="null"/></exception>
+        public static CurrentSectionStageRepository GetInstance()
+        {
+            return GetInstance<CurrentSectionStageRepository>(SessionWrapper.GetInstance().Factory);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stage"></param>
+        /// <returns></returns>
+        public IEnumerable<CurrentSectionStage> GetLimits(Guid stage)
+        {
+
+            return GetAll()
+                .Where(st => st.Stage.ID == stage)
+                .Select(st => new CurrentSectionStage());
+        }
+    }
+}
