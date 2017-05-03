072b86445c49da6b5f01970e4da1b1c90350ef00
Marina Vasilieva <marina_paley@mail.ru>
2017-05-03 15:56:36 +0300

    Pull1ModeRusi4

---
+++ TrainMovement/Interpolation/Pull1Rusi4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ORM.Trains.Interpolation.Entities;
+using ORM.Trains.Entities;
+using ORM.Trains.Repository.Interpolation;
+
+
+namespace TrainMovement.Interpolation
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class Pull1Rusi4 : BaseModeRusi4
+    {
+        /// <exception cref="ArgumentNullException">value is <see langword="null"/></exception>
+        internal Pull1Rusi4(MassMass mass)
+        {
+            ForceAndCurrent = VFIRepository.GetVfiRusi4Pull1(mass);
+        }
+    }
+}
