f239db68929d4a3569e7657a57d4e5d065253e2c
Marina Vasilieva <marina_paley@mail.ru>
2017-05-03 16:00:38 +0300

    VFIRepository

--- ORM/Trains/Repository/Interpolation/VFIRepository.cs
+++ ORM/Trains/Repository/Interpolation/VFIRepository.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using ORM.Base;
 using ORM.Trains.Entities;
 using ORM.Trains.Interpolation.Entities;
+using ORM.Trains.Repository.Trains;
 
 namespace ORM.Trains.Repository.Interpolation
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class VFIRepository : Repository<VFI>
 
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public static VFIRepository GetInstance()
         {
             return GetInstance<VFIRepository>(SessionWrapper.GetInstance().Factory);
@@ -21,7 +29,7 @@ namespace ORM.Trains.Repository.Interpolation
         /// <param name="powerSupplyStation"></param>
         /// <param name="name"></param>
         /// <returns></returns>
-        public IList<VFI> GetVFI(String trainName, ModeControls motorcontrol, MassMass mass)
+        public IEnumerable<VFI> GetVFI(String trainName, ModeControls motorcontrol, MassMass mass)
         {
             return GetAll()
                  .Where(vfi => vfi.Train.Name == trainName)
@@ -29,6 +37,17 @@ namespace ORM.Trains.Repository.Interpolation
                  .Where(vfi => vfi.Mass == mass)
                  .Select(vfi => vfi).ToList();
         }
-         
+
+        /// <exception cref="ArgumentNullException">Значение параметра <paramref name="source" /> или <paramref name="predicate" /> — null.</exception>
+        public static IEnumerable<VFI> GetVfiRusi4Pull1(MassMass mass)
+        {
+            var modecontrolRepository = ModeControlsRepository.GetInstance();
+            var modecontrol = modecontrolRepository.GetByModeControl("Pull1");
+            const String trainName = "81-740.1(Rusi4)";
+            var vfiRepository = GetInstance();
+
+            return vfiRepository.GetVFI(trainName, modecontrol, mass);
+        }
+
     }
 }
